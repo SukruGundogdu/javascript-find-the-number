bf3cbad3ab012ad09e35ed6e02610d3dc38cd10e
sukru <sukrugundogdudatascience@gmail.com>
2020-10-08 16:29:06 +0300

    finish coding of find the number

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const random_number = Math.floor(Math.random() * 100 + 1);
+let random_number = Math.floor(Math.random() * 100 + 1);
 // console.log(random_number);
 document.querySelector("#check_it").addEventListener("click", checkNumber)
 document.querySelector("#nr_game").addEventListener("keyup", function(event){
@@ -12,27 +12,40 @@ function checkNumber(){
     // input elentine tekrar tekrar daha rahat erişim sağlamak için
    let userInputElement = document.querySelector("#nr_game");
    let answerLabelElement = document.querySelector("#answer");
+   let guessLabelElement = document.querySelector("#count_it");
 
 
 
    if(userInputElement.value == random_number){
        alert("You are the winner! Guess time: "+ ++guess_count);
-       guess_count = 0;
+       
 
        const choice = confirm("Game agein?");     // tekrar oynamak istersem true istemezsem false dondurur
-       console.log(choice);                  
+
+       if(choice){
+        guess_count = 0; 
+        random_number = Math.floor(Math.random() * 100 + 1);
+        answerLabelElement.innerText = "";
+       
+       } else {
+           alert("Thanks for palaying!");
+           answerLabelElement.innerText = "Best guess time: " + guess_count;
+           guessLabelElement.innerText = "";
+       }
+
+    //    console.log(choice);                  
    } 
    else if(userInputElement.value == ""){      
     answerLabelElement.innerText = "Enter a number!";
     }
    else if(userInputElement.value > random_number) {
         guess_count++;
-        document.querySelector("#count_it").innerText = `Guess count is: ${guess_count}`;
+        guessLabelElement.innerText = `Guess count is: ${guess_count}`;
         answerLabelElement.innerText = "Try a smaller number!";
    }
    else if (userInputElement.value < random_number) {
         guess_count++;
-        document.querySelector("#count_it").innerText = `Guess count is: ${guess_count}`;
+        guessLabelElement.innerText = `Guess count is: ${guess_count}`;
         answerLabelElement.innerText = "Try a higher number!";
    }
    else if(isNaN(userInputElement.value)){
